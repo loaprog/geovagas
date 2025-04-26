009349c7a431ddc920070086180f13851a878fd9
loaprog <loa_ava@hotmail.com>
2025-04-26 04:18:39 -0300

    Adiciona suporte a variáveis de ambiente para configuração de porta e URL do banco de dados; atualiza links em templates HTML

--- meu-projeto/go.mod
+++ meu-projeto/go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.60.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
--- meu-projeto/internal/database/db.go
+++ meu-projeto/internal/database/db.go
@@ -4,10 +4,17 @@ import (
 	"context"  // Importando o pacote context
 	"log"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"os"
+	"fmt"
 )
 
 func NewDB(ctx context.Context) (*pgxpool.Pool, error) {  
-    databaseUrl := "postgresql://neondb_owner:npg_TgAy3E1baFtK@ep-aged-glitter-ackvv1y3-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
+    // databaseUrl := "postgresql://neondb_owner:npg_TgAy3E1baFtK@ep-aged-glitter-ackvv1y3-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require"
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		log.Fatal("A variável de ambiente DATABASE_URL não está definida.")
+		return nil, fmt.Errorf("DATABASE_URL não está definida")
+	}
 
 	db, err := pgxpool.New(ctx, databaseUrl)  
 	if err != nil {
